fc22f00479eaf0ed75717031356dd57c6f1cbb6d
galfrylich <galfrylich@gmail.com>
2022-04-07 16:57:52 +0300

    create 3 path

--- monday-quiz-game/src/app/app-routing.module.ts
+++ monday-quiz-game/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EndQuizComponent } from './end-quiz/end-quiz.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { QuestionComponent } from './question/question.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch:"full"},
+  {path:"home", component:HomepageComponent},
+  {path:"question", component:QuestionComponent},
+  {path:"endQuiz", component:EndQuizComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
