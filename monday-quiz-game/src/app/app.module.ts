650ccc4d9e9b283a3dbfdcbfd33cbf9532ffcf6c
galfrylich <galfrylich@gmail.com>
2022-04-09 22:39:37 +0300

    module

--- monday-quiz-game/src/app/app.module.ts
+++ monday-quiz-game/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { QuestionComponent } from './question/question.component';
+import { HeaderComponent } from './header/header.component';
+import { ButtonChangeDirective } from './button-change.directive';
+import { EndQuizComponent } from './end-quiz/end-quiz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    QuestionComponent,
+    HeaderComponent,
+    ButtonChangeDirective,
+    EndQuizComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
