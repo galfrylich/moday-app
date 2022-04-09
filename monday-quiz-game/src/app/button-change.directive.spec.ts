829fb9ee6425c403657f6d97742caa02311a29ac
galfrylich <galfrylich@gmail.com>
2022-04-09 22:38:08 +0300

    directive

---
+++ monday-quiz-game/src/app/button-change.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ButtonChangeDirective } from './button-change.directive';
+
+describe('ButtonChangeDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ButtonChangeDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ monday-quiz-game/src/app/button-change.directive.ts
@@ -0,0 +1,21 @@
+import { flatten } from '@angular/compiler';
+import { Directive,ElementRef,Input, HostListener,Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appButtonChange]'
+})
+export class ButtonChangeDirective {
+  @Input() correctAnswer : boolean = false;
+  constructor(private el: ElementRef,private render : Renderer2) { }
+
+  @HostListener('click') clickAnswer() {
+    console.log(this.correctAnswer);
+    if(this.correctAnswer){
+      this.render.addClass(this.el.nativeElement,'btn-outline-success');
+      this.correctAnswer =false;
+    }else{
+      this.render.addClass(this.el.nativeElement,'btn-outline-danger');
+    }
+    
+  }
+}
